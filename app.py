c121cfba9a49c410b166c2f0e6d5a8c6829b689b
DarkieDonno <skstamp@clemson.edu>
2026-02-21 13:11:29 -0500

    specified port number

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ def get_db_connection():
         host=os.getenv("DB_HOST"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASS"),
-        database=os.getenv("DB_NAME")
+        database=os.getenv("DB_NAME"),
+        port=int(os.getenv("DB_PORT", 3306))
     )
 
 @app.route('/health', methods=['GET'])
